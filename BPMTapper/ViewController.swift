f47370e9c5bf9ca6f68dfe7304d59b0c13026b2d
Tyler Neveldine <tneveldine@gmail.com>
2017-05-02 21:59:32 -0700

    feat(tapper): add the tapper and the logic for computing BPM and show a visualization

--- BPMTapper/ViewController.swift
+++ BPMTapper/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var BPMLabel: UILabel!
+    @IBOutlet var tapButton: UIButton!
+    
+    var startTimer : Timer!
+    var trackingTimer : Timer!
+    var previousTapTime = Date()
+    var numberOfTaps = 0.0
+    var averageBPM = 0.0
+    
+    @IBAction func tapButtonWasPressed(_ sender: Any)
+    {
+        let timeDifference = -previousTapTime.timeIntervalSinceNow
+        
+        if (timeDifference > 5)
+        {
+            self.numberOfTaps = 0
+            self.averageBPM = 0
+        }
+        
+        let BPM = 60 / timeDifference
+        
+        if (numberOfTaps < 4)
+        {
+            numberOfTaps += 1
+        }
+        
+        if (numberOfTaps == 1)
+        {
+            averageBPM = BPM
+        }
+        else
+        {
+            averageBPM = (averageBPM * ((numberOfTaps - 1) / numberOfTaps)) + BPM / numberOfTaps
+        }
+        
+        BPMLabel.text = "\(averageBPM)"
+        previousTapTime = Date()
+        
+        if (startTimer != nil)
+        {
+            startTimer.invalidate()
+        }
+        if (trackingTimer != nil)
+        {
+            trackingTimer.invalidate()
+        }
+        startTimer = Timer.scheduledTimer(withTimeInterval: (60 / averageBPM), repeats: false, block: { (timer) in
+            self.startTrackingBPM()
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func startTrackingBPM()
+    {
+        trackingTimer = Timer.scheduledTimer(withTimeInterval: (60 / averageBPM), repeats: true) { (timer) in
+            if (self.view.backgroundColor == UIColor.red)
+            {
+                self.view.backgroundColor = UIColor.white
+            }
+            else
+            {
+                self.view.backgroundColor = UIColor.red
+            }
+        }
     }
-
-
+    
 }
 
